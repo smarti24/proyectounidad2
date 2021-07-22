ecd85ff416145bbca123c99119699ff15100e984
smarti24 <sergioalejandro0895@gmail.com>
2021-07-21 20:20:50 -0500

    Proyecto Diplomado Unidad 2

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(".textojs").mouseover(function(){
+    $(this).css("background" , "red")
+})
+$(".textojs").mouseout(function(){
+    $(this).css("background" , "white")
+})
+
+
+$(".subtitulosjs").mouseover(function(){
+    $(this).css("font-size" , "60px")
+})
+$(".subtitulosjs").mouseout(function(){
+    $(this).css("font-size" , "")
+})
+
+$(".parrafojs").mouseover(function(){
+    $(this).css("font-size" , "40px")
+})
+$(".parrafojs").mouseout(function(){
+    $(this).css("font-size" , "")
+})
